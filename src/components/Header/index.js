d53fa948bdaf9b63198f8f3168108bd0ce9a0c2e
ZQWhiting <zach.whiting@icloud.com>
2020-09-13 21:10:59 -0600

    added Navigation Component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,41 +1,16 @@
 import React from 'react';
+import Navigation from '../Navigation';
 import './style.css';
 
 function Header(props) {
 	const { selectedSection, setSelectedSection } = props;
-	const navLinks = [
-		{ name: 'About Me' },
-		{ name: 'Portfolio' },
-		{ name: 'Contact' },
-		{ name: 'Resume' },
-	];
 	return (
 		<section>
 			<h1>Zachary Q. Whiting</h1>
-			<nav>
-				<ul>
-					{navLinks.map(({ name }) => {
-						name = name.split(' ');
-						return (
-							<li key={name[0]}>
-								<a
-									href={`#${name[0]}`}
-									className={
-										selectedSection === name[0]
-											? 'isActive'
-											: undefined
-									}
-									onClick={() =>
-										setSelectedSection(`${name[0]}`)
-									}
-								>
-									<span>{name.join(' ')}</span>
-								</a>
-							</li>
-						);
-					})}
-				</ul>
-			</nav>
+			<Navigation
+				selectedSection={selectedSection}
+				setSelectedSection={setSelectedSection}
+			/>
 		</section>
 	);
 }
