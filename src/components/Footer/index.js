880a187a9445e1f2ba0501ff9b107c2110223e06
ZQWhiting <zach.whiting@icloud.com>
2020-09-13 20:31:00 -0600

    added links to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,7 +2,22 @@ import React from 'react';
 import './style.css';
 
 function Footer() {
-	return null;
+	return (
+		<section>
+			<a href='https://github.com/ZQWhiting' target='_blank'>
+				GitHub
+			</a>
+			<a href='https://www.linkedin.com/in/zqwhiting' target='_blank'>
+				LinkedIn
+			</a>
+			<a
+				href='https://www.facebook.com/zachary.whiting.796'
+				target='_blank'
+			>
+				FaceBook
+			</a>
+		</section>
+	);
 }
 
 export default Footer;
