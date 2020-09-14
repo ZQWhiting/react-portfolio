8cb56269c37a1ed8141e96ea0421781385022a9a
ZQWhiting <zach.whiting@icloud.com>
2020-09-13 19:27:03 -0600

    rendered Header with navigation and highlighted selected section

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,41 @@
 import React from 'react';
+import './index.css';
 
-function Header() {
-	return null;
+function Header(props) {
+	const { selectedSection, setSelectedSection } = props;
+	const navLinks = [
+		{ name: 'About' },
+		{ name: 'Portfolio' },
+		{ name: 'Contact' },
+		{ name: 'Resume' },
+	];
+	return (
+		<header>
+			Zachary Q. Whiting
+			<nav>
+				<ul>
+					{navLinks.map((link) => {
+						return (
+							<li>
+								<a
+									href={`#${link.name}`}
+									className={
+										selectedSection === link.name &&
+										'isActive'
+									}
+									onClick={() =>
+										setSelectedSection(`${link.name}`)
+									}
+								>
+									<span>{link.name}</span>
+								</a>
+							</li>
+						);
+					})}
+				</ul>
+			</nav>
+		</header>
+	);
 }
 
 export default Header;
