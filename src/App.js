6c7427e8ed92333492d998808b3f5d4df029d585
ZQWhiting <zach.whiting@icloud.com>
2020-09-14 23:45:52 -0600

    added logo and some header styling

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 	const [selectedSection, setSelectedSection] = useState('About');
 	return (
 		<>
-			<header>
+			<header className='header'>
 				<Header
 					selectedSection={selectedSection}
 					setSelectedSection={setSelectedSection}
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Navigation from '../Navigation';
 import './style.css';
+import logo from '../../assets/images/Logo.png';
 
 function Header(props) {
 	const { selectedSection, setSelectedSection } = props;
+	const [menuSelected, setMenuSelected] = useState(false)
+
 	return (
-		<section>
-			<h1>Zachary Q. Whiting</h1>
-			<Navigation
-				selectedSection={selectedSection}
-				setSelectedSection={setSelectedSection}
-			/>
-		</section>
+		<>
+			<div className='logo-container'>
+				<img
+					src={logo}
+					alt='Zachary Q Whiting. Full-Stack Developer'
+				></img>
+			</div>
+			<div className='nav-bar'>
+				<div className='nav-menu'>
+					<button className='nav-menu-btn' onClick={() => setMenuSelected(!menuSelected)}>
+						Nav
+					</button>
+					{menuSelected && (<div className='nav-menu-content'>
+						<Navigation
+							selectedSection={selectedSection}
+							setSelectedSection={setSelectedSection}
+						/>
+					</div>)}
+				</div>
+			</div>
+		</>
 	);
 }
 
