558c338de2b65abedbf27abaefe12d0c5d84d0f4
ZQWhiting <zach.whiting@icloud.com>
2020-09-15 19:10:55 -0600

    fixed nav-menu sticky error

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -11,30 +11,33 @@ function Header(props) {
 
 	return (
 		<>
-			<header className='header' onClick={() => setMenuSelected(!menuSelected)}>
-					<div></div>
-					<div className='logo-container'>
-						<img
-							src={logo}
-							alt='Zachary Q Whiting. Full-Stack Developer'
-						></img>
-					</div>
-					<div className='nav-btn-container' >
-						<button
-							className={`nav-menu-btn ${
-								menuSelected ? 'isActive' : ''
-							}`}
-						>
-							<FontAwesomeIcon icon={faBars} alt='navigation menu'/>
-						</button>
-					</div>
+			<header
+				className='header'
+				onClick={() => setMenuSelected(!menuSelected)}
+			>
+				<div></div>
+				<div className='logo-container'>
+					<img
+						src={logo}
+						alt='Zachary Q Whiting. Full-Stack Developer'
+					></img>
+				</div>
+				<div className='nav-btn-container'>
+					<button
+						className={`nav-menu-btn ${
+							menuSelected ? 'isActive' : ''
+						}`}
+					>
+						<FontAwesomeIcon icon={faBars} alt='navigation menu' />
+					</button>
+				</div>
+				{menuSelected && (
+					<Navigation
+						selectedSection={selectedSection}
+						setSelectedSection={setSelectedSection}
+					/>
+				)}
 			</header>
-			{menuSelected && (
-				<Navigation
-					selectedSection={selectedSection}
-					setSelectedSection={setSelectedSection}
-				/>
-			)}
 		</>
 	);
 }
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -10,28 +10,24 @@ function Navigation(props) {
 		{ name: 'Resume' },
 	];
 	return (
-		<nav className='nav-menu'>
-			<ul className='nav-menu-content'>
-				{navLinks.map(({ name }) => {
-					name = name.split(' ');
-					return (
-						<li className='nav-link' key={name[0]}>
-							<a
-								href={`#${name[0]}`}
-								className={
-									selectedSection === name[0]
-										? 'isActive'
-										: ''
-								}
-								onClick={() => setSelectedSection(`${name[0]}`)}
-							>
-								<span>{name.join(' ')}</span>
-							</a>
-						</li>
-					);
-				})}
-			</ul>
-		</nav>
+		<ul className='nav-menu'>
+			{navLinks.map(({ name }) => {
+				name = name.split(' ');
+				return (
+					<li className='nav-link' key={name[0]}>
+						<a
+							href={`#${name[0]}`}
+							className={
+								selectedSection === name[0] ? 'isActive' : ''
+							}
+							onClick={() => setSelectedSection(`${name[0]}`)}
+						>
+							<span>{name.join(' ')}</span>
+						</a>
+					</li>
+				);
+			})}
+		</ul>
 	);
 }
 
