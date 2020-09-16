0b12ac0eef9c70e0a998b80c0235bae4a8fa471c
ZQWhiting <zach.whiting@icloud.com>
2020-09-15 20:54:52 -0600

    styled Resume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -2,24 +2,35 @@ import React from 'react';
 import './style.css';
 
 function Resume() {
-	const proficiencies = [
+	const frontEndProficiencies = [
 		{ name: 'MongoDB' },
 		{ name: 'Express.js' },
 		{ name: 'React.js' },
 		{ name: 'Node.js' },
 	];
+	const backEndProficiencies = [{ name: 'Apis' }];
 	return (
-		<section>
+		<section id='resume'>
 			{/* link to resume */}
 			<h2>Resume</h2>
-			<a href='#Resume'>Resume</a>
+			<a href='#Resume'>Download my resume</a>
 			{/* a list of developer's proficiencies */}
-			<h3>Proficiencies:</h3>
-			<ul>
-				{proficiencies.map(({ name }) => (
-					<li key={name}>{name}</li>
-				))}
-			</ul>
+			<div>
+				<h3>Front-end Proficiencies:</h3>
+				<ul>
+					{frontEndProficiencies.map(({ name }) => (
+						<li key={name}>{name}</li>
+					))}
+				</ul>
+			</div>
+			<div>
+				<h3>Back-end Proficiencies:</h3>
+				<ul>
+					{backEndProficiencies.map(({ name }) => (
+						<li key={name}>{name}</li>
+					))}
+				</ul>
+			</div>
 		</section>
 	);
 }
