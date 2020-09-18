c669f91b9ee1fb9f8a0ecd8a62b40969c1c1ee95
ZQWhiting <zach.whiting@icloud.com>
2020-09-17 21:53:12 -0600

    added project contents to portfolio

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,11 +3,7 @@ import photo from '../../assets/images/me.jpg';
 import './style.css';
 
 function About() {
-	const bioText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eleifend finibus odio id finibus. Praesent imperdiet nec nulla convallis consequat. Curabitur vestibulum massa dui, in posuere elit rutrum vel. Etiam est libero, bibendum vitae odio eget, condimentum commodo leo. Proin vitae ipsum ligula. Integer dictum dapibus iaculis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris non pharetra erat, vitae vulputate leo. Vivamus laoreet lorem sed faucibus egestas. Nulla posuere nibh pharetra lorem egestas sagittis. Cras elementum leo vel malesuada dapibus. Cras pulvinar est eu eleifend interdum. Ut id massa ut magna posuere egestas.\r\n
-
-	Donec vel nulla convallis, rutrum justo non, venenatis mauris. Aenean non commodo arcu. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam arcu mi, accumsan quis elementum eget, ultrices in sem. Vestibulum non lobortis odio, eu imperdiet mauris. Donec ac nisi at magna faucibus lacinia. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aliquam justo tortor, sagittis sit amet lacus quis, faucibus condimentum metus. Integer in nulla id leo varius elementum vitae et leo. Quisque volutpat massa urna, a vestibulum neque euismod nec. Phasellus nec purus dignissim, volutpat nisi ut, sagittis dui. Duis et diam non erat porttitor tempus. Nam aliquet varius tempor.\r\n
-
-	Integer viverra nulla maximus fermentum ultrices. Sed consequat suscipit arcu, ornare dictum erat gravida at. Curabitur non risus bibendum, dictum risus in, efficitur massa. Aliquam non nisi in nulla imperdiet auctor vel non nisi. Suspendisse facilisis sem gravida tortor pretium, sed pulvinar tellus porta. Nulla sit amet nisi lorem. Quisque tincidunt nisi lectus, fringilla ornare neque laoreet ut. Suspendisse pretium mattis sapien, non suscipit velit commodo vitae. Vestibulum quis porta ligula, non ultrices lorem. Duis eu felis ac justo convallis euismod. Aliquam ultricies, ante in tristique vehicula, eros ligula consectetur orci, eget consectetur lacus nibh quis sem.`;
+	const bioText = ``;
 
 	return (
 		<section>
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,31 +1,51 @@
 import React from 'react';
 import './style.css';
 import Project from '../Project';
-import img1 from '../../assets/images/runbuddy.jpg';
-import img2 from '../../assets/images/puzzle.jpg';
-import img3 from '../../assets/images/calendar.jpg';
-import img4 from '../../assets/images/Calculator.jpg';
-import img5 from '../../assets/images/LED.jpg';
-import img6 from '../../assets/images/surf.jpg';
+import img1 from '../../assets/images/media4u.jpg';
+import img2 from '../../assets/images/budget_tracker.jpg';
+import img3 from '../../assets/images/photo_port.jpg';
+import img4 from '../../assets/images/food_festival.jpg';
+import img5 from '../../assets/images/weather_dashboard.jpg';
+import img6 from '../../assets/images/tech_blog.jpg';
 
 function Portfolio() {
 	const projects = [
 		{
-			title: 'Hello',
+			title: 'Media4U',
 			image: img1,
-			deployed: 'deployed',
-			repo: 'repo',
+			deployed: 'https://zqwhiting.github.io/media4u/',
+			repo: 'http://github.com/zqwhiting/media4u',
 		},
-		{ title: 'ABC', image: img5, deployed: 'deployed', repo: 'repo' },
 		{
-			title: 'World',
+			title: 'Budget Tracker',
 			image: img2,
-			deployed: 'deployed',
-			repo: 'repo',
+			deployed: 'https://zqwhiting-budget-tracker-pwa.herokuapp.com/',
+			repo: 'http://github.com/zqwhiting/budget-tracker-pwa',
 		},
-		{ title: 'DFE', image: img3, deployed: 'deployed', repo: 'repo' },
-		{ title: 'FAD', image: img4, deployed: 'deployed', repo: 'repo' },
-		{ title: 'fdsa', image: img6, deployed: 'deployed', repo: 'repo' },
+		{
+			title: 'Photo Port',
+			image: img3,
+			deployed: 'https://zqwhiting.github.io/photo-port/',
+			repo: 'http://github.com/zqwhiting/photo-port',
+		},
+		{
+			title: 'Food Festival',
+			image: img4,
+			deployed: 'https://zqwhiting.github.io/food-festival/',
+			repo: 'http://github.com/zqwhiting/food-festival',
+		},
+		{
+			title: 'Weather Dashboard',
+			image: img5,
+			deployed: 'https://zqwhiting.github.io/weather-dashboard/',
+			repo: 'http://github.com/zqwhiting/weather-dashboard',
+		},
+		{
+			title: 'Tech Blog',
+			image: img6,
+			deployed: 'https://zqw-tech-blog.herokuapp.com/',
+			repo: 'http://github.com/zqwhiting/tech-blog',
+		},
 	];
 	return (
 		<section>
