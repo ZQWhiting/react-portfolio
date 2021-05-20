8e44b9d2e04438654099646e605d2fbd7ed31271
ZQWhiting <zach.whiting@icloud.com>
2021-05-19 22:05:33 -0600

    resume page styling adjustments

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -18,19 +18,23 @@ function Resume() {
 			<h1>
 				<span>Resume</span>
 			</h1>
-			<a href={pdfResume} rel='noopener noreferrer' target='_blank'>
-				Link to Resume.pdf
-				<FontAwesomeIcon icon={faExternalLinkAlt}></FontAwesomeIcon>
-			</a>
-			<a
-				href={wordResume}
-				download='ZQWhitingResume.docx'
-				rel='noopener noreferrer'
-				target='_blank'
-			>
-				Download Resume.docx
-				<FontAwesomeIcon icon={faFileDownload}></FontAwesomeIcon>
-			</a>
+			<h2>
+				<a href={pdfResume} rel='noopener noreferrer' target='_blank'>
+					Resume.pdf
+					<FontAwesomeIcon icon={faExternalLinkAlt}></FontAwesomeIcon>
+				</a>
+			</h2>
+			<h2>
+				<a
+					href={wordResume}
+					download='ZQWhitingResume.docx'
+					rel='noopener noreferrer'
+					target='_blank'
+				>
+					Resume.docx
+					<FontAwesomeIcon icon={faFileDownload}></FontAwesomeIcon>
+				</a>
+			</h2>
 			<div>
 				<h2>Proficiencies:</h2>
 				<ul id='proficiencies'>
